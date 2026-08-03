f91346714c8ebdae60c8add02140a8bdcbe55f49
HamzzWkwk <ahmadfarhan2000a@gmail.com>
2026-08-03 14:18:01 +0700

    lord

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function show() {
-  let tes = document.getElementById("click");
-  tes.innerHTML = "Work Coy"
-}
+const btn = document.getElementById("click");
+let text = document.getElementById("text");
+
+btn.addEventListener("click", function () {
+  btn.classList.toggle("active");
+
+  if (btn.classList.contains("active")) {
+    btn.innerHTML = "Kembali";
+    text.innerHTML = "5191T";
+  } else {
+    btn.innerHTML = "Klik Gue";
+    text.innerHTML = "LocoVerse Website";
+  }
+});
